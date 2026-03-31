3e0a03a966f4ed692fdf40e0bdc67159abda5a4f
dearnasaruddin <dearnasaruddin@gmail.com>
2026-03-31 21:56:12 +0600

    mongodb redis cloudinary checking added in health check controller

--- controller/auth/registrationController.js
+++ controller/auth/registrationController.js
@@ -1,6 +1,6 @@
 const bcrypt = require('bcryptjs')
 const User = require('../../model/userModel')
-const { emailQueue } = require('../../config/queueConfig')
+// const { emailQueue } = require('../../config/queueConfig')
 
 const registrationController = async (req, res) => {
 
@@ -41,13 +41,13 @@ const registrationController = async (req, res) => {
 
     try {
         await user.save()
-        // res.send({message: 'Registration Successful! Please Log in.'})
-        await emailQueue.add('verifyEmail',user,{
-            attempts: 5,
-            backoff: 5000,
-            removeOnComplete: true
-        })
-        res.send({message: 'Registration Successful! Please check your email for verification.'})
+        res.send({message: 'Registration Successful! Please Log in.'})
+        // await emailQueue.add('verifyEmail',user,{
+        //     attempts: 5,
+        //     backoff: 5000,
+        //     removeOnComplete: true
+        // })
+        // res.send({message: 'Registration Successful! Please check your email for verification.'})
     } catch (error) {
         console.log("While trying to save data in database " + error)
         res.send({error: 'Something went wrong! Registration failed!'})
--- controller/healthCheckController.js
+++ controller/healthCheckController.js
@@ -1,5 +1,76 @@
-const healthCheckController = (req, res) => {
-    res.send('ami health check')
+const { createClient } = require("ioredis")
+const mongoose = require("mongoose")
+const cloudinary = require('cloudinary').v2;
+
+const healthCheckController = async (req, res) => {
+    const health = {
+        status: 'ok',
+        timestamps: new Date(),
+        uptime: process.uptime(),
+        services: {
+            mongodb: 'unknown',
+            redis: 'unknown',
+            cloudinary: 'unknown',
+        }
+    }
+
+    try {
+        const dbState = mongoose.connection.readyState
+        health.services.mongodb = dbState === 1 ? 'connected' : 'disconnected'
+    } catch (error) {
+        health.services.mongodb = {
+            status: 'disconnected',
+            error: error
+        }
+    }
+
+    try {
+        const redisClient = createClient({
+            url: `redis://:${process.env.REDIS_PASSWORD}@${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`
+        })
+
+        redisClient.on("error", (err) => {
+            health.services.redis = {
+                status: 'disconnected',
+                error: err
+            }
+        })
+
+        await redisClient.connect();
+        const pingResponse = await redisClient.ping();
+
+        if (pingResponse === 'PONG') {
+            health.services.redis = 'connected';
+        } else {
+            throw new Error('Redis ping failed');
+        }
+        await redisClient.disconnect();
+        // redisClient.on("ready", () => {
+        //     health.services.redis = 'connected'
+        // })
+
+    } catch (error) {
+        health.services.redis = {
+            status: 'disconnected',
+            error: error
+        }
+    }
+
+    try {
+        const response = await cloudinary.api.ping();
+        if (response.status === 'ok') {
+            health.services.cloudinary = 'connected'
+        }
+
+    } catch (error) {
+        health.services.cloudinary = {
+            status: 'disconnected',
+            error: error
+        }
+    }
+
+    res.send(health)
+
 }
 
 module.exports = healthCheckController
